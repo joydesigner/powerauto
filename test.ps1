611682fc2d96416441d987f3cc5d0aa4e1b47b93
xin zheng <jason.zhengxin@gmail.com>
2025-04-13 17:07:22 +1000

    remove the test script

--- test.ps1
+++
@@ -1,9 +0,0 @@
-#region users hash table
-$users = @{
-  'abertram' = 'Adam Bertram'
-  'bcarter'  = 'Bob Carter'
-  'cdoe'     = 'Charlie Doe'
-}
-#endregion
-
-Write-Output "abertram's full name is $($users['abertram'])"
